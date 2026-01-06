461ee39a1bb61074eb57c939c0e2e510c58239c0
alexrus1144 <alexrus1144@gmail.com>
2026-01-06 14:46:12 +0000

    fix progectid

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import * as admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      projectId: "relaxdev-af44c",
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       // Исправление для переносов строк в приватном ключе
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
